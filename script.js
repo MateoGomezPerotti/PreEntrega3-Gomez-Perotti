09927c2978b6f37ea835c9bf53b7d861dbef7933
mateoperotti <mateogomezperotti@gmail.com>
2024-02-15 20:40:06 -0300

    agregando DOM

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-let nombreUsuario = prompt("Ingresa tu nombre");
-let edad = prompt("Ingresa tu edad");
-let ciudad = prompt("Ingresa tu ciudad");
+// let nombreUsuario = prompt("Ingresa tu nombre");
+// let edad = prompt("Ingresa tu edad");
 
-let persona = {
-  nombre: nombreUsuario,
-  edad: edad,
-  ciudad: ciudad,
-  saludar: function() {
-    return "¡Hola! " + this.nombre + " ahora te hare unas preguntas para calcular tu IMC ";
-  }
-};
+// let persona = {
+//   nombre: nombreUsuario,
+//   edad: edad,
+//   saludar: function() {
+//     return "¡Hola! " + this.nombre + " ahora te hare unas preguntas para calcular tu IMC ";
+//   }
+// };
 
-alert(persona.saludar());
+// alert(persona.saludar());
 
-function calcularIMC(peso, altura) {
-    let imc = peso / (altura * altura);
-    return imc;
-  }
+  function calcularIMC() {
+    let peso = parseFloat(document.getElementById('peso').value);
+    let altura = parseFloat(document.getElementById('altura').value);
+    let resultadoIMC = peso / (altura * altura);
+    let resultado = "Tu IMC es: " + resultadoIMC;
   
-  let peso = parseFloat(prompt("Ingresa tu peso en kilogramos:"));
-  let alturaEnMetros = parseFloat(prompt("Ingresa tu altura en metros:"));
-
-  alert("ahora te hare una pregunta de cultura mientras la pagina calcula tu IMC")
-
-  
-  let cultura = prompt("elegi la mejor pelicula de Shrek: \n 1- Shrek 1 \n 2- Shrek 2 \n 3- Shrek tercero \n 4- Shrek para siempre")
-  switch(cultura){
-
-    case "1":
-        alert("Elegiste una buena opción, pero no la mejor")
-        break;
-    case "2":
-        alert("Definitivamente la mejor pelicula de DreamWorks")
-        break;
-    case "3":
-        alert("No me lo puedo creer")
-        break;
-    case "4":
-        alert("Ni cerca")
-    default:
-        alert("elegiste una opcion no valida")
-        break;
-
+  document.getElementById("resultado").innerHTML = resultado;
   }
 
-  let resultadoIMC = calcularIMC(peso, alturaEnMetros);
-  alert("Gracias por la espera, tu índice de masa corporal es: " + resultadoIMC);
- 
-let imc = prompt("ingrese el IMC que correspondiente ");
+  // alert("ahora te hare una pregunta de cultura mientras la pagina calcula tu IMC")
+  
+  // let cultura = prompt("elegi la mejor pelicula de Shrek: \n 1- Shrek 1 \n 2- Shrek 2 \n 3- Shrek tercero \n 4- Shrek para siempre")
+  // switch(cultura){
+
+  //   case "1":
+  //       alert("Elegiste una buena opción, pero no la mejor")
+  //       break;
+  //   case "2":
+  //       alert("Definitivamente la mejor pelicula de DreamWorks")
+  //       break;
+  //   case "3":
+  //       alert("No me lo puedo creer")
+  //       break;
+  //   case "4":
+  //       alert("Ni cerca")
+  //   default:
+  //       alert("elegiste una opcion no valida")
+  //       break;
+
+  // }
+
+// let imc = prompt("ingrese el IMC que correspondiente ");
 
 // Función para determinar la rutina en base al IMC
 function determinarRutina(imc) {
@@ -62,9 +57,6 @@ function determinarRutina(imc) {
 
 
 let rutinaRecomendada = determinarRutina(imc);
-
-
-alert("La rutina recomendada es: " + rutinaRecomendada);
 
 let rutinasDeEjercicio = [
     {
@@ -83,9 +75,8 @@ let rutinasDeEjercicio = [
       ejercicios: ["Carrera continua", "Ciclismo", "Entrenamiento HIIT", "Ejercicios cardiovasculares"]
     }
   ];
-  
-  let rutina1 = prompt("Por favor, ingrese su IMC:");
-  let rutinasFiltradas;
+
+let rutinasFiltradas;
   
   if (imc < 18.5) {
     rutinasFiltradas = rutinasDeEjercicio.filter(function(rutina) {
