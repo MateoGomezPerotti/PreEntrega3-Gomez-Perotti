7fb589a3ae6fcead8d65e162559b36584717d1f1
mateoperotti <mateogomezperotti@gmail.com>
2024-02-18 19:38:00 -0300

    agregando DOM, JSON

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-// let nombreUsuario = prompt("Ingresa tu nombre");
-// let edad = prompt("Ingresa tu edad");
+function guardarDatos(){
+  let nombreUsuario = document.getElementById('exampleInputName1').value;
+  let contraseñaUsuario = document.getElementById('exampleInputPassword1').value;
 
-// let persona = {
-//   nombre: nombreUsuario,
-//   edad: edad,
-//   saludar: function() {
-//     return "¡Hola! " + this.nombre + " ahora te hare unas preguntas para calcular tu IMC ";
-//   }
-// };
+  let user = {
+    nombreUsuario: nombreUsuario,
+    contraseñaUsuario : contraseñaUsuario
+  };
 
-// alert(persona.saludar());
+  let usuarioJSON = JSON.stringify(user);
+
+  localStorage.setItem('datosUsuario', usuarioJSON);
+
+  alert('Datos guardados con éxito')
+
+  document.getElementById('userName').innerHTML = "¡Hola, " + nombreUsuario + "!";
+
+}
+
+let usuarioJSON = localStorage.getItem('datosUsuario');
+  if (usuarioJSON) {
+    let user = JSON.parse(usuarioJSON);
+    document.getElementById('userName').innerHTML = "¡Hola, " + user.nombreUsuario + "!";
+  }
+
+  function cerrarSesion(){
+    localStorage.removeItem('datosUsuario');
+    document.getElementById('userName').innerHTML = "Usuario";
+  }
+  
 
   function calcularIMC() {
     let peso = parseFloat(document.getElementById('peso').value);
@@ -18,78 +36,19 @@
     let resultado = "Tu IMC es: " + resultadoIMC;
   
   document.getElementById("resultado").innerHTML = resultado;
+  let rutinaRecomendada = determinarRutina(resultadoIMC);
+  document.getElementById('rutina').innerHTML = "tu rutina recomendada es: " + rutinaRecomendada;
+
   }
 
-  // alert("ahora te hare una pregunta de cultura mientras la pagina calcula tu IMC")
-  
-  // let cultura = prompt("elegi la mejor pelicula de Shrek: \n 1- Shrek 1 \n 2- Shrek 2 \n 3- Shrek tercero \n 4- Shrek para siempre")
-  // switch(cultura){
-
-  //   case "1":
-  //       alert("Elegiste una buena opción, pero no la mejor")
-  //       break;
-  //   case "2":
-  //       alert("Definitivamente la mejor pelicula de DreamWorks")
-  //       break;
-  //   case "3":
-  //       alert("No me lo puedo creer")
-  //       break;
-  //   case "4":
-  //       alert("Ni cerca")
-  //   default:
-  //       alert("elegiste una opcion no valida")
-  //       break;
-
-  // }
-
-// let imc = prompt("ingrese el IMC que correspondiente ");
-
-// Función para determinar la rutina en base al IMC
-function determinarRutina(imc) {
-  if (imc < 18.5) {
-    return "Rutina para aumentar masa muscular";
-  } else if (imc >= 18.5 && imc < 25) {
-    return "Rutina de mantenimiento";
-  } else {
-    return "Rutina para perder peso";
-  }
-}
-
-
-let rutinaRecomendada = determinarRutina(imc);
-
-let rutinasDeEjercicio = [
-    {
-      nombre: "Rutina para aumentar masa muscular",
-      descripcion: "Esta rutina está diseñada para aquellos que desean aumentar su masa muscular.",
-      ejercicios: ["Press de banca", "Sentadillas", "Dominadas", "Fondos en paralelas"]
-    },
-    {
-      nombre: "Rutina de mantenimiento",
-      descripcion: "Esta rutina es ideal para mantener tu condición física actual.",
-      ejercicios: ["Caminata ligera", "Natación", "Yoga", "Ejercicios de estiramiento"]
-    },
-    {
-      nombre: "Rutina para perder peso",
-      descripcion: "Esta rutina está enfocada en la quema de grasa y la pérdida de peso.",
-      ejercicios: ["Carrera continua", "Ciclismo", "Entrenamiento HIIT", "Ejercicios cardiovasculares"]
+  function determinarRutina(resultado) {
+    let rutina = "";
+    if (resultado < 18.5) {
+      rutina = "Rutina para aumentar masa muscular";
+    } else if (resultado >= 18.5 && resultado < 25) {
+      rutina = "Rutina de mantenimiento";
+    } else {
+      rutina = "Rutina para perder peso";
     }
-  ];
-
-let rutinasFiltradas;
-  
-  if (imc < 18.5) {
-    rutinasFiltradas = rutinasDeEjercicio.filter(function(rutina) {
-      return rutina.nombre.toLowerCase().includes("aumentar");
-    });
-  } else if (imc >= 18.5 && imc < 25) {
-    rutinasFiltradas = rutinasDeEjercicio.filter(function(rutina) {
-      return rutina.nombre.toLowerCase().includes("mantenimiento");
-    });
-  } else {
-    rutinasFiltradas = rutinasDeEjercicio.filter(function(rutina) {
-      return rutina.nombre.toLowerCase().includes("perder peso");
-    });
+    return rutina;
   }
-  
-  console.log(rutinasFiltradas);
